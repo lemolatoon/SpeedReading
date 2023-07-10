7f46f66262c779bb5ff4c61e906cb85166bd2145
kaisei <khishida55@gmail.com>
2023-07-10 18:25:59 +0900

    Fix useCallback deps

--- src/App.js
+++ src/App.js
@@ -19,13 +19,13 @@ function App() {
   const callback = useCallback(() => {
     if (playing) {
       console.log("a");
-      if (showing.current + 3 > text.length)
+      if (showing.current + 3 > orig.length)
       setText(orig.slice(showing.current, showing.current + 5));
       showing.current += 5;
     } else {
       showing.current = 0;
     }
-  }, [text, playing, showing, setText]);
+  }, [playing, showing, setText, orig]);
   useInterval(callback, 300);
   const onChange = (text) => {
     setOrig(text)
